6cd4d8cae6e8b31eddcb774e806b073117120f19
Bryce Kerley <bkerley@brycekerley.net>
2010-12-24 13:45:17 -0500

    more words

--- ludlum.rb
+++ ludlum.rb
@@ -1,5 +1,5 @@
-NAMES = %w{ Adams Clancy Stross Beckett Rand Ludlum }
-THINGS = %w{ Gambit Conspiracy Codex Morgue Edge }
+NAMES = %w{ Adams Clancy Stross Beckett Rand Ludlum Baker MacAllan Rowling Stoker Tyndale }
+THINGS = %w{ Gambit Conspiracy Codex Morgue Edge Memorandum Situation Mention }
 get '/' do
   n = NAMES[rand(NAMES.length)]
   t = THINGS[rand(THINGS.length)]
