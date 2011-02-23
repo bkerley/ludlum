d7bf805111245b2e0f7d99c0e49b981854e37078
Bryce Kerley <bkerley@brycekerley.net>
2011-02-23 15:47:50 -0500

    alice as noun

--- ludlum.rb
+++ ludlum.rb
@@ -1,4 +1,4 @@
-NAMES = %w{ Adams Clancy Stross Beckett Rand Ludlum Baker MacAllan Rowling Stoker Tyndale Sobol Stephenson Lightner Baumann Clinton Avenir Zapf Braun Gamma Beta Alpha Omega Waterhouse Moscow Berlin Munich Washington Portland Foucault Francis Oliver Young }
+NAMES = %w{ Adams Clancy Stross Beckett Rand Ludlum Baker MacAllan Rowling Stoker Tyndale Sobol Stephenson Lightner Baumann Clinton Avenir Zapf Braun Gamma Beta Alpha Omega Waterhouse Moscow Berlin Munich Washington Portland Foucault Francis Oliver Young Alice }
 THINGS = %w{ Gambit Conspiracy Codex Morgue Edge Memorandum Situation Mention Dream Nation Point Force Agenda Minute Break Bottle Book Death Circumflex Chancery Razor Factor Clause Colony Incident Deception Inception File Cabinet Criterion Ultimatum Identity Palimpsest Pendulum Tragedy Collective Paperweight Account Credenza Bureau Seat Chair Kindling Task }
 get '/' do
   n = NAMES[rand(NAMES.length)]
